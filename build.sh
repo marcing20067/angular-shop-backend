8bffda84fae45bfa851ae5d12443f49159881c13
Marcin8013 <marcing2067@wp.pl>
2022-05-22 10:57:39 +0200

    Add build sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+npm install @angular/cli -g
+
+#!/usr/bin/env bash
+
+git clone https://github.com/marcing20067/angular-shop.git ./frontend
+
+cd frontend
+
+npm install --production
+
+npm install --dev
+
+ng build
