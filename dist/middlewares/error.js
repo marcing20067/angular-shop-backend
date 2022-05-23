b33da74957d26e4471ffefc293064ade0c811ca4
Marcin8013 <marcing2067@wp.pl>
2022-05-23 22:08:36 +0200

    Delete dist content

--- dist/middlewares/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ErrorHandler = (err, req, res, next) => {
-    console.log(err);
-    res.status(err.statusCode || 500).send({
-        message: err.errorMessage || 'Unhandled error occurred.',
-    });
-};
-exports.default = ErrorHandler;
--- dist/utils/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.utmte.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-exports.default = mongoose_1.default.connect(uri);
